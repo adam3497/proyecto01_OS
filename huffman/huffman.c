10ffeb09a34827de3d9a3886187328b84440e5a5
adam3497 <amador19972012@gmail.com>
2024-03-23 17:03:45 -0600

    Modificación al algoritmo de Huffman para usar wide characters

--- huffman/huffman.c
+++ huffman/huffman.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
 
 // Structure for a Huffman tree node
 struct MinHeapNode {
-    char data;
+    wchar_t data;
     unsigned freq;
     struct MinHeapNode *left, *right;
 };
@@ -16,7 +17,7 @@ struct MinHeap {
 };
 
 // Function to create a new Min Heap node
-struct MinHeapNode* newNode(char data, unsigned freq) {
+struct MinHeapNode* newNode(wchar_t data, unsigned freq) {
     struct MinHeapNode* temp = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
     temp->left = temp->right = NULL;
     temp->data = data;
@@ -42,20 +43,26 @@ void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b) {
 
 // Function to heapify at a given index
 void minHeapify(struct MinHeap* minHeap, int idx) {
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
+    int smallest = idx; // Parent node
+    int left = 2 * idx + 1; // Left child node
+    int right = 2 * idx + 2; // Right child node
 
+    // Check of the left child node has a smaller frequency than its parent node
     if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq) {
+        // If so, the left child node is the smallest
         smallest = left;
     }
 
+    // Check if the right child node has a smaller frequency than its parent node
     if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq) {
+        // If so, the right child node is the smallest
         smallest = right;
     }
 
+    // If any of the above conditions was satisfied, we swap positions
     if (smallest != idx) {
-        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]); 
+        // We do it recursively
         minHeapify(minHeap, smallest);
     }
 }
@@ -76,17 +83,24 @@ struct MinHeapNode* extractMin(struct MinHeap* minHeap) {
 
 // Function to insert a new node to Min Heap
 void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode) {
-    ++minHeap->size;
+    ++minHeap->size; // Increment the min heap size
+    // Index to the last position of the heap array
     int i = minHeap->size - 1;
+    // Compares the frequency of the new node with its parent node's frequency
+    // with (i - 1) / 2 we get the parent for i-th
     while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq) {
-        minHeap->array[i] = minHeap->array[(i - 1) / 2];
-        i = (i - 1) / 2;
+        // If the frequency of the new node is less than its parent's, it swaps 
+        // the new node with its parent
+        minHeap->array[i] = minHeap->array[(i - 1) / 2]; 
+        i = (i - 1) / 2; // Set i to be the position of i's parent node 
     }
+    // Once either the new node becomes the root or its frequency is not lees than its parent's
+    // We insert the new node to the i position result
     minHeap->array[i] = minHeapNode;
 }
 
 // Function to build the Huffman Tree
-struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size) {
+struct MinHeapNode* buildHuffmanTree(wchar_t data[], int freq[], int size) {
     struct MinHeapNode *left, *right, *top;
     struct MinHeap* minHeap = createMinHeap(size);
     for (int i = 0; i < size; ++i)
@@ -95,7 +109,7 @@ struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size) {
     while (!isSizeOne(minHeap)) {
         left = extractMin(minHeap);
         right = extractMin(minHeap);
-        top = newNode('$', left->freq + right->freq);
+        top = newNode(L'$', left->freq + right->freq);
         top->left = left;
         top->right = right;
         insertMinHeap(minHeap, top);
@@ -116,7 +130,7 @@ void printCodes(struct MinHeapNode* root, int arr[], int top) {
     }
 
     if (!(root->left) && !(root->right)) {
-        printf("%c: ", root->data);
+        wprintf(L"%lc: ", root->data);
         for (int i = 0; i < top; ++i)
             printf("%d", arr[i]);
         printf("\n");
@@ -124,18 +138,19 @@ void printCodes(struct MinHeapNode* root, int arr[], int top) {
 }
 
 // Function to build Huffman codes
-void HuffmanCodes(char data[], int freq[], int size) {
+void HuffmanCodes(wchar_t data[], int freq[], int size) {
     struct MinHeapNode* root = buildHuffmanTree(data, freq, size);
     int arr[100], top = 0;
     printCodes(root, arr, top);
 }
 
-/* // Driver program to test above functions
+// Driver program to test above functions
 int main() {
-    char data[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'j'};
+    wchar_t data[] = { L'a', L'b', L'c', L'd', L'e', L'f', L'j'};
     int freq[] = { 5, 9, 120, 13, 16, 45, 98 };
     int size = sizeof(data) / sizeof(data[0]);
+    printf("size is %d\n", size);
     printf("Huffman codes:\n");
     HuffmanCodes(data, freq, size);
     return 0;
-} */
+}
