2faf19b79c7c3a6fbb0b4609298e9d358d0a41ff
sjcr23 <jd.castro23r@gmail.com>
2024-04-04 14:04:04 -0600

    removing pid_t from shared memory and making setting offset before decompress

--- src/fork/decompressor.c
+++ src/fork/decompressor.c
@@ -14,10 +14,9 @@
 #include "../huffman/huffman.c"
 #include "file_locks.c"
 
-#define MAX_CONCURRENT_PROCESSES 4 // Set the maximum number of concurrent processes
+#define MAX_CONCURRENT_PROCESSES 1 // Set the maximum number of concurrent processes
 
 int main() {
-    pid_t pid;
     int numOfProcess = TOTAL_BOOKS;
 
     // Folder Paths
@@ -37,31 +36,36 @@ int main() {
     const char* dir_result = create_output_dir(dirMetadata.directory);
     const char* dir_path = concat_strings(dir_result, "/");
 
-
     // Track the number of active processes
     int activeProcesses = 0; 
     int miliseconds = 150;
-    
+
     // Decode 
     for (int i = 0; i < numOfProcess; i++) {
+        
         // Wait until there are available process slots
         while (activeProcesses >= MAX_CONCURRENT_PROCESSES) {
-            wait(NULL); // Wait for any child process to finish
+            wait(NULL); 
             activeProcesses--;
         }
 
-        pid = fork();
-        usleep(miliseconds * 1000);
-
+        pid_t pid = fork();
+        
         // Código específico del proceso hijo
         if (pid == 0) {
-            parallel_decompress(binary_source, dir_path, i+1, dirMetadata.offsets[i]);
+            // Move to file to decompress
+            size_t currentOffset = dirMetadata.offsets[i];
+            fseek(binary_source, currentOffset, SEEK_SET);
+            decompress_and_write_to_file(binary_source, dir_path, i+1);
+            printf("[PID %d][FINISHED #%d]\n", getpid(), i+1);
+
             return 0;
         } else if (pid > 0) {
-            activeProcesses++; // Increment active process count in the parent
+            activeProcesses++;
         } else {
             // Error handling for fork failure
             fprintf(stderr, "Fork failed.\n");
+            exit(EXIT_FAILURE);
         }
     }
     
